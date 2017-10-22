3873cd5ac35e2e29ad0c4f94adc7fb3186be2d35
Daniel <dan.laframb@gmail.com>
2017-10-21 23:50:47 -0400

    Looping over users of main page of searched hashtag and printing data

--- webscrapper/py/scrapper.py
+++ webscrapper/py/scrapper.py
@@ -1,9 +1,9 @@
 import sys
 import time
-import requests
-import threading
 from bs4 import BeautifulSoup
 from selenium import webdriver
+
+
 
 # Chrome driver executable is in py directory, no need to specify path
 driver = webdriver.Chrome()
@@ -15,23 +15,36 @@ driver.get(url + str(sys.argv[1]) + "/")
 
 links = [a.get_attribute('href') for a in driver.find_elements_by_css_selector('div._f2mse a')]
 
-driver.get(links[0])
+# print (links)
 
-r = requests.get(links[0])
 
-data = r.text
+for link in range(4):
+	# Create driver
+	driver2 = webdriver.Chrome()
+	# Go to URL of user
+	driver2.get(links[link])
+	# Get user's URL
+	username = [a.get_attribute('href') for a in driver2.find_elements_by_css_selector('div._eeohz a')]
+	# Go to user's profile
+	driver2.get(username[0])
+	# Print username from profile page
+	print (driver2.find_element_by_class_name('_rf3jb').text)
+	# Loop over Posts, Followers and Following
+	for elem in driver2.find_elements_by_xpath('//span[@class = "_fd86t"]'):
+		# Print Posts, Followers, Following
+		print (elem.text)
 
-soup = BeautifulSoup(data, "lxml")
-print soup
-for link in soup.find_all('a'):
-	print(link.get('href'))
 
-# def worker(link): 
-# 	# Thread worker function
+# driver.get(links[0])
 
-# threads = []
+# username = [a.get_attribute('href') for a in driver.find_elements_by_css_selector('div._eeohz a')]
 
-# for i in range(5):
-# 	t = threading.Thread(target=worker, args=(i,))
-# 	threads.append(t)
-# 	t.start()
+# driver.get(username[0])
+
+# print ("Printing username: ")
+
+# print (driver.find_element_by_class_name('_rf3jb').text)
+
+# # Number of posts, followers following
+# for elem in driver.find_elements_by_xpath('//span[@class = "_fd86t"]'):
+# 	print (elem.text)
